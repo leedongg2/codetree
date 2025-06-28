aae487dd4e4704e8903d9d0eb7053e9e05f9651d
leedongg2 <168533414+leedongg2@users.noreply.github.com>
2025-06-29 01:03:31 +0900

    Title: 구역 청소, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/구역 청소/district-cleaning.cpp
+++ 250629/구역 청소/district-cleaning.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <algorithm>
+#include <stdbool.h>
 using namespace std;
 
-int g[5];
-
+int g[5],f;
+bool che[101]={false};
 int main() {
+    int ans=0;
     cin>>g[0]>>g[1]>>g[2]>>g[3];
-    sort(g,g+4);
-    cout<<g[3]-g[0];
-
+    for(f=g[0];f<g[1];f++){
+        che[f]=true;
+    }
+    for(f=g[2];f<g[3];f++){
+        che[f]=true;
+    }
+    for(f=0;f<101;f++){
+        if(che[f]){
+            ans++;
+        }
+    }
+    cout<<ans;
     return 0;
 }

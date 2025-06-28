e3b12cc4bb1007799f0730e03fefa20b96e820b1
leedongg2 <168533414+leedongg2@users.noreply.github.com>
2025-06-29 00:38:30 +0900

    Title: 두 선분, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250629/두 선분/two-lines.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int x[5];
+    for(int f=0;f<4;f++){
+        cin>>x[f];
+    }
+    if(x[1]>=x[2] || x[3]>=x[0]){
+        cout<<"intersecting";
+        return 0;
+    }
+    cout<<"nonintersecting";
+
+}

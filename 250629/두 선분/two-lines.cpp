80267a096d2b31e040aefeefa2cf4bc72fa12391
leedongg2 <168533414+leedongg2@users.noreply.github.com>
2025-06-29 00:45:28 +0900

    Title: 두 선분, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/두 선분/two-lines.cpp
+++ 250629/두 선분/two-lines.cpp
@@ -2,14 +2,12 @@
 #include <algorithm>
 using namespace std;
 int main(){
-    int x[5];
-    for(int f=0;f<4;f++){
-        cin>>x[f];
-    }
-    if(x[1]>=x[2] || x[3]<=x[0]){
+    int x[3],y[3];
+    cin>>x[0]>>x[1]>>y[0]>>y[1];
+    if((x[0]>=y[0] && x[0]<=y[1]) || (x[1]>=y[0] && x[1]<=y[1]) || (y[0]>=x[0] && y[0]<=x[1]) || (y[1]>=x[0] && y[1]<=x[1])){
         cout<<"intersecting";
-        return 0;
     }
-    cout<<"nonintersecting";
-
+    else{
+        cout<<"nonintersecting";
+    }
 }

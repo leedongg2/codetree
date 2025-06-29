552a7f95cc509cc6d5ad2c382b8ea0114b0d0a7c
leedongg2 <168533414+leedongg2@users.noreply.github.com>
2025-06-29 10:17:03 +0900

    Title: 전부 겹치는 선분, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 250629/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
-int f,n;
-int x1[101], x2[101];
-
-int main() {
-    cin >> n;
-    int ma1=0, ma2=0;
-    for (int i = 0; i < n; i++) {
-        cin >> x1[i] >> x2[i];
-    }
-    for(f=0;f<n;f++){
-        if(x1[f]>ma1){
-            ma1=x1[f];
+int main(){
+    int a,b,c,d,e,f,g[101]={0};
+    cin>>a;
+    for(f=0;f<a;f++){
+        cin>>b>>c;
+        for(e=b;e<=c;e++){
+            g[e]++;
         }
     }
-     for(f=0;f<n;f++){
-        if(x2[f]>ma2){
-            ma2=x2[f];
+    for(f=0;f<101;f++){
+        if(g[f]==a){
+            cout<<"Yes";
+            return 0;
         }
     }
-    if(ma1<=ma2){
-        cout<<"Yes";
-        return 0;
-    }
-    cout<<"NO";
+    cout<<"No";
 }
